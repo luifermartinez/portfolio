c19e9338e7f299eacd2633d05e73ff4f2a67d852
luifermartinez <thelm@hotmail.es>
2024-02-03 21:24:38 -0400

    Add GitHub Pages deployment workflow and update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
+  site: "https://luifermartinez.github.io",
+  base: "/portfolio",
 });
