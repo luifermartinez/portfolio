a2cbf8d9e530e281b7f3e2283f373e69ffd23666
Luis Martínez <thelm@hotmail.es>
2024-04-12 13:45:27 -0400

    Update Astro integrations and base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte()],
+  integrations: [svelte()],
   site: "https://luifermartinez.github.io",
   base: '/portfolio',
   output: 'static',
